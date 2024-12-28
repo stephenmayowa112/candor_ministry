92456e56bf214ac6d0f617042a289a75ff509dcf
stephenmayowa112 <stephenmayowa112@gmail.com>
2024-12-28 12:18:52 +0100

    refactor: reorder RecentAudioVisuals component and update import statements in Navbar

--- my-website/src/app/page.tsx
+++ my-website/src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Hero />
       <DownloadBanner />
       <RecentBooks />
-      <RecentAudioVisuals />
       <SupportOptions />
+      <RecentAudioVisuals />
     </>
   )
 }
